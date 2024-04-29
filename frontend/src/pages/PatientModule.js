973ceb7d0fba8f6c512990fedabbcd84d46ed231
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-04-29 22:22:20 +0530

    updated

--- frontend/src/pages/PatientModule.js
+++ frontend/src/pages/PatientModule.js
@@ -9,7 +9,6 @@ const PatientModule = () => {
             <h1 className="text-3xl font-bold mb-4">Patient Module</h1>
             <MeetingOptions />
             <NearbyHospitals />
-            <DoctorProfile />
         </div>
     );
 };
